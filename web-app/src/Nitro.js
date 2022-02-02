dbb28253b887c17d6b3bc2c5fb7cd79ef645d759
Vivek Nair <vivek@nair.me>
2022-02-02 15:38:23 -0800

    textbox functionality

--- web-app/src/Nitro.js
+++ web-app/src/Nitro.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Dropzone from 'react-dropzone';
 import Cert from './components/Cert';
 import Attribute from './components/Attribute';
+const Buffer = require('buffer/').Buffer
 
 class Nitro extends React.Component {
   constructor(props) {
     super(props)
     this.state = {result: null}
     this.onUpload = this.onUpload.bind(this)
-    this.verifyAttestation = this.verifyAttestation.bind(this)
     this.retry = this.retry.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.verifyAttestation = this.verifyAttestation.bind(this)
   }
 
   retry() {
@@ -18,7 +20,6 @@ class Nitro extends React.Component {
 
   async verifyAttestation(attestationDocument) {
     const attestationResult = await window.trust.enclaves.nitro.verifyAttestation(attestationDocument)
-    console.log(attestationResult)
     this.setState({result: attestationResult})
   }
 
@@ -31,11 +32,21 @@ class Nitro extends React.Component {
     }
   }
 
+  onChange(e) {
+    if (/[0-9a-fA-F]+/.test(e.target.value)) {
+      const buffer = Buffer.from(e.target.value, 'hex')
+      this.verifyAttestation(buffer)
+    } else {
+      const buffer = Buffer.from(e.target.value, 'base64')
+      this.verifyAttestation(buffer)
+    }
+  }
+
   render() {
     return (<>
       <div className="page-header mb-4">
         <h1>AWS Nitro Tools</h1>
-        <p className="lead">Validate AWS Nitro attestations and encrypt secrets for use in Nitro enclaves. Upload an attestation document in CBOR format or paste a base64-encoded document below to begin. Sample attestation documents are available here: &nbsp;<a className="link" href="https://github.com/multifactor/trust-center/tree/main/test/examples" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt" />&nbsp;&thinsp;GitHub Repo</a></p>
+        <p className="lead">Validate AWS Nitro attestations and encrypt secrets for use in Nitro enclaves. Upload an attestation document in CBOR format or paste a base64 or hex-encoded document below to begin. Sample attestation documents are available here: &nbsp;<a className="link" href="https://github.com/multifactor/trust-center/tree/main/test/examples" target="_blank" rel="noreferrer"><i className="fas fa-external-link-alt" />&nbsp;&thinsp;GitHub Repo</a></p>
       </div>
       <div className="row pt-4">
         <div className="col-6">
@@ -95,7 +106,7 @@ class Nitro extends React.Component {
               <button type="button" className="btn btn-secondary" onClick={this.retry}><i className="fas fa-sync-alt" />&nbsp; Retry</button>
             </>
           }</> : <div className="form-group">
-            <textarea className="form-control position-absolute h-100" placeholder="Or, paste a base64-encoded attestation document here"></textarea>
+            <textarea onChange={this.onChange} className="form-control position-absolute h-100" placeholder="Or, paste a base64 or hex-encoded attestation document here"></textarea>
           </div>}
         </div>
       </div>
